df247cd1d14d67e6785fd6ebb11644953d1df76f
Feast <feast107@gmail.com>
2024-03-22 17:19:04 +0800

    Update

--- OpenSlideNET.Tool/Program.cs
+++ OpenSlideNET.Tool/Program.cs
@@ -14,5 +14,22 @@ Console.WriteLine(JsonSerializer.Serialize(args, new JsonSerializerOptions
 if (args.Length < 2) throw new ArgumentException($"command args should be at least 2");
 var input  = args[0];
 var output = args[1];
-TiffPageDeleter.NdpiDeleteMacro(input, output);
+if (Directory.Exists(input))
+{
+    if (Path.HasExtension(output)) throw new ArgumentException("If specified as directory, output should be directory too");
+    if (!Directory.Exists(output)) Directory.CreateDirectory(output);
+    foreach (var file in Directory.EnumerateFiles(input))
+    {
+        TiffPageDeleter.NdpiDeleteMacro(file, Path.Combine(output, Path.GetFileName(file)));
+    }
+}
+else if (File.Exists(input))
+{
+    TiffPageDeleter.NdpiDeleteMacro(input, output);
+}
+else
+{
+    throw new ArgumentException("input should be a file or directory");
+}
+
 Console.WriteLine("输出完成");
